812abb03abda74874a3bce0d71a6fc07aa235948
zettlrobert <zettl.robert@gmail.com>
2023-01-09 19:01:57 +0100

    comment: regarding api keys, template literals, call stack, error handling and fallbacks

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -9,9 +9,15 @@ export default function Weather() {
     if (city === null || city === "") {
       return;
     }
+
+    // WARN: NEVER EVER store your api keys in your source code
+    // Youse your environemnt for that, process.env or in case of firebae deployment [secret parameters](https://firebase.google.com/docs/functions/config-env#secret_parameters)
     const apiKey = "854e09de099a5dd32d428062a572a45f";
     const unit = "metric";
     const lang = "de";
+    // NOTE: A template literal would be much more easy to read than string concatenation
+    const _exampleTemplateLiteral = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}&lang=${lang}`;
+
     const apiUrl =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
@@ -21,6 +27,10 @@ export default function Weather() {
       unit +
       "&lang=" +
       lang;
+
+    // NOTE: Comparing template literal and concatenated string
+    console.log(_exampleTemplateLiteral === apiUrl);
+
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
@@ -30,6 +40,8 @@ export default function Weather() {
           setWeather(data);
         }
       });
+
+    // NOTE this value is not available at the time of loging it, your handle click is a synchrounus function, the fetch method will return a promise immidieately after beeing called, the handleClick execution resumes without waiting for the resolved fetch... rendering the data does work, because you only update the local state after the promise has resolved -> move the console log into the else block of your fetch or use async/await as mentioned in `Jokes.jsx`
     console.log(weather.weather);
   }
 
@@ -52,6 +64,7 @@ export default function Weather() {
           <h2>Temperatur: {weather.main.temp} C</h2>
           <h2> {weather.weather[0].description}</h2>
           <div>
+            {/* NOTE: There are multiple issues here, the image will not receive the weatherIcon properties on a slow internet connection and that error is not really handled (fallback image/loading spinner) furthermore it is not really necessary to pass the src url where you concatenate the icon onto, as that always happens in the component you should scope it to the component */}
             <WeatherIcon
               weatherIcon={weather.weather[0].icon}
               src="https://openweathermap.org/img/wn/"
